a3dfc506d2ac4c71ec625fdf7478d1ce88f23c10
Keivin98 <kevinisufaj@gmail.com>
2025-10-15 12:36:04 +0300

    schedule and speakers fallbacks

--- resources/views/sections/schedule.blade.php
+++ resources/views/sections/schedule.blade.php
@@ -5,36 +5,40 @@
       <p>Here is our event schedule</p>
     </div>
 
-    <ul class="nav nav-tabs" role="tablist">
-      @foreach($schedules as $key => $day)
-        <li class="nav-item">
-          <a class="nav-link{{ $key === 1 ? ' active' : '' }}" href="#day-{{ $key }}" role="tab" data-toggle="tab">Day {{ $key }}</a>
-        </li>
-      @endforeach
-    </ul>
+    @if($schedules->count() > 0)
+      <ul class="nav nav-tabs" role="tablist">
+        @foreach($schedules as $key => $day)
+          <li class="nav-item">
+            <a class="nav-link{{ $key === 1 ? ' active' : '' }}" href="#day-{{ $key }}" role="tab" data-toggle="tab">Day {{ $key }}</a>
+          </li>
+        @endforeach
+      </ul>
 
-    <h3 class="sub-heading">Voluptatem nulla veniam soluta et corrupti consequatur neque eveniet officia. Eius
-      necessitatibus voluptatem quis labore perspiciatis quia.</h3>
-
-    <div class="tab-content row justify-content-center">
-      @foreach($schedules as $key => $day)
-        <div role="tabpanel" class="col-lg-9 tab-pane fade{{ $key === 1 ? ' show active' : '' }}" id="day-{{ $key }}">
-          @foreach($day as $schedule)
-            <div class="row schedule-item">
-              <div class="col-md-2"><time>{{ \Carbon\Carbon::parse($schedule->start_time)->format("h:i A") }}</time></div>
-              <div class="col-md-10">
-                @if($schedule->speaker)
-                  <div class="speaker">
-                    <img src="{{ $schedule->speaker->photo->getUrl() }}" alt="{{ $schedule->speaker->name }}">
-                  </div>
-                @endif
-                <h4>{{ $schedule->title }} @if($schedule->speaker)<span>{{ $schedule->speaker->name }}</span>@endif</h4>
-                <p>{{ $schedule->subtitle }}</p>
+      <div class="tab-content row justify-content-center">
+        @foreach($schedules as $key => $day)
+          <div role="tabpanel" class="col-lg-9 tab-pane fade{{ $key === 1 ? ' show active' : '' }}" id="day-{{ $key }}">
+            @foreach($day as $schedule)
+              <div class="row schedule-item">
+                <div class="col-md-2"><time>{{ \Carbon\Carbon::parse($schedule->start_time)->format("h:i A") }}</time></div>
+                <div class="col-md-10">
+                  @if($schedule->speaker)
+                    <div class="speaker">
+                      <img src="{{ $schedule->speaker->photo->getUrl() }}" alt="{{ $schedule->speaker->name }}">
+                    </div>
+                  @endif
+                  <h4>{{ $schedule->title }} @if($schedule->speaker)<span>{{ $schedule->speaker->name }}</span>@endif</h4>
+                  <p>{{ $schedule->subtitle }}</p>
+                </div>
               </div>
-            </div>
-          @endforeach
-        </div>
-      @endforeach
-    </div>
+            @endforeach
+          </div>
+        @endforeach
+      </div>
+    @else
+      <div class="text-center" style="padding: 60px 20px; color: #666;">
+        <h4>Event schedule will be populated here</h4>
+        <p>Check back soon for our detailed event schedule!</p>
+      </div>
+    @endif
   </div>
 </section>
--- resources/views/sections/speakers.blade.php
+++ resources/views/sections/speakers.blade.php
@@ -6,17 +6,26 @@
     </div>
 
     <div class="row">
-      @foreach($speakers as $speaker)
-        <div class="col-lg-4 col-md-6">
-          <div class="speaker">
-            <img src="{{ $speaker->photo->getUrl() }}" alt="{{ $speaker->name }}" class="img-fluid speaker-img">
-            <div class="details">
-              <h3><a href="{{ route('speaker', $speaker->id) }}">{{ $speaker->name }}</a></h3>
-              <p>{{$speaker->description}}</p>
+      @if($speakers->count() > 0)
+        @foreach($speakers as $speaker)
+          <div class="col-lg-4 col-md-6">
+            <div class="speaker">
+              <img src="{{ $speaker->photo->getUrl() }}" alt="{{ $speaker->name }}" class="img-fluid speaker-img">
+              <div class="details">
+                <h3><a href="{{ route('speaker', $speaker->id) }}">{{ $speaker->name }}</a></h3>
+                <p>{{$speaker->description}}</p>
+              </div>
             </div>
           </div>
+        @endforeach
+      @else
+        <div class="col-12">
+          <div class="text-center" style="padding: 60px 20px; color: #666;">
+            <h4>List of speakers will be populated here</h4>
+            <p>Check back soon for our amazing lineup of speakers!</p>
+          </div>
         </div>
-      @endforeach
+      @endif
     </div>
   </div>
 </section>
